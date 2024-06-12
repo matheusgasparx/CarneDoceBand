5cae881fe271d8f48d8c94ce65be46af3c19fba0
matheusgasparx <139236157+matheusgasparx@users.noreply.github.com>
2024-06-12 00:05:09 -0300

    update: All Project

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,8 +8,15 @@ module.exports = {
         'cerere': "url('/src/assets/cerere-back.png')",
         'pequi': "url('/src/assets/pequifundo.jpg')",
         'tonus': "url('/src/assets/tonus3.png')",
-        'princesa': "url('/src/assets/princesafundo.png')"
+        'princesa': "url('/src/assets/princesafundo.png')",
+        'carnedoce': 'linear-gradient(90deg, rgba(205,100,0,1) 5%, rgba(119,53,0,1) 50%, rgba(205,100,0,1) 95%)' 
       },
+      width: {
+        'custom-w': '700px' 
+      },
+      height: {
+        'custom-h': '865px'
+      }
   },
   plugins: [],
 }
