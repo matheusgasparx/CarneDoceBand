4111fbef11929a3d091c15b1d29dfd8be3f80aab
matheusgasparx <139236157+matheusgasparx@users.noreply.github.com>
2024-06-20 19:59:13 -0300

    :pencil2:

--- src/script.js
+++ src/script.js
@@ -2,16 +2,24 @@ window.sr = ScrollReveal({ reset: true })
 
 sr.reveal('.reveal', {
     distance: '50px',
-    duration: 1000,
+    duration: 1500,
     easing: 'ease-in-out',
     origin: 'bottom',
 })
 
+function showOverlay(id) {
+    document.getElementById(id).classList.add('active')
+}
+
+function hideOverlay(id) {
+    document.getElementById(id).classList.remove('active')
+}
+
 ScrollReveal().reveal('.logo', {
-    duration: 800,
-    easing: 'ease-in-out',
-    origin: 'center',
+    origin: 'top',
+    distance: '50px',
+    duration: 1500,
     reset: false,
     opacity: 0,
-    scale: 0.85
-});
+    easing: 'ease-in-out'
+})
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,8 @@ module.exports = {
         'princesa': "url('/src/assets/princesafundo.png')",
         'carnedocecover': "url('/src/assets/carnedocealbumcover.jpg')",
         'carnedocealbum': "url('/src/assets/carnedocealbum.jpg')",
-        'carnedoce': 'linear-gradient(90deg, rgba(205,100,0,1) 5%, rgba(119,53,0,1) 50%, rgba(205,100,0,1) 95%)' 
+        'carnedoce': 'linear-gradient(90deg, rgba(205,100,0,1) 5%, rgba(119,53,0,1) 50%, rgba(205,100,0,1) 95%)',
+        'rodape': 'radial-gradient(circle, rgba(122,86,58,1) 43%, rgba(179,105,44,1) 54%, rgba(119,81,50,1) 77%)' 
       },
       width: {
         'custom-w': '700px' 
