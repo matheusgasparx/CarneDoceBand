962839f554096e884f8a73104e7139ce892b0d65
matheus gaspar <contatomatheusgaspar@gmail.com>
2025-04-11 17:33:22 -0300

    :trollface: joke /

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,13 +4,13 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        salmaeyes: "url('/assets/cd.jpg')",
-        cerere: "url('/assets/cerere-back.png')",
-        pequi: "url('/assets/pequifundo.jpg')",
-        tonus: "url('/assets/tonus3.png')",
-        princesa: "url('/assets/princesafundo.png')",
-        carnedocecover: "url('/assets/carnedocealbumcover.jpg')",
-        carnedocealbum: "url('/assets/carnedocealbum.jpg')",
+        salmaeyes: "url('assets/cd.jpg')",
+        cerere: "url('assets/cerere-back.png')",
+        pequi: "url('assets/pequifundo.jpg')",
+        tonus: "url('assets/tonus3.png')",
+        princesa: "url('assets/princesafundo.png')",
+        carnedocecover: "url('assets/carnedocealbumcover.jpg')",
+        carnedocealbum: "url('assets/carnedocealbum.jpg')",
       }
     },
   },
