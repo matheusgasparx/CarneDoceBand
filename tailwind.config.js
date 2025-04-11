db6946d05a5355142d915be81e27efa4fbc17822
matheus gaspar <contatomatheusgaspar@gmail.com>
2025-04-11 17:19:07 -0300

    :factory: update/ organization

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./**/*.{html,js}"],
+  content: ["./**/*.html", "./**/*.js"],
   theme: {
     extend: {
       backgroundImage: {
-        salmaeyes: "url('/assets/cd.jpg')",
-        cerere: "url('/assets/cerere-back.png')",
-        pequi: "url('/assets/pequifundo.jpg')",
-        tonus: "url('/assets/tonus3.png')",
-        princesa: "url('/assets/princesafundo.png')",
-        carnedocecover: "url('/assets/carnedocealbumcover.jpg')",
-        carnedocealbum: "url('/assets/carnedocealbum.jpg')",
-      },
+        salmaeyes: "url('assets/cd.jpg')",
+        cerere: "url('assets/cerere-back.png')",
+        pequi: "url('assets/pequifundo.jpg')",
+        tonus: "url('assets/tonus3.png')",
+        princesa: "url('assets/princesafundo.png')",
+        carnedocecover: "url('assets/carnedocealbumcover.jpg')",
+        carnedocealbum: "url('assets/carnedocealbum.jpg')",
+      }
     },
   },
   plugins: [],
